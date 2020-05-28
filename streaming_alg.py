95f01b2c0ba707cf97b6924ff5bbc0e8c257feb3
Catalina Vajiac <catvajiac@gmail.com>
2020-05-28 13:19:12 -0400

    use sklearn tfidf

--- streaming_alg.py
+++ streaming_alg.py
@@ -3,9 +3,8 @@
 # Purpose:  Use LSH idea to cluster ad data
 # Usage:    ./streaming_alg.py [filename]
 
+import re
 import math
-import heapq
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import os, sys
@@ -16,13 +15,15 @@ import time
 
 from collections import Counter, defaultdict
 from datetime import datetime
-from itertools import groupby
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from sortedcontainers import SortedList
+from sklearn.metrics.cluster import adjusted_rand_score, homogeneity_score, normalized_mutual_info_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from itertools import combinations
 
 
 class hash_family():
-    def __init__(self, num_hash_functions=32, buckets_in_hash=1000):
+    def __init__(self, num_hash_functions, buckets_in_hash):
         self.num_hash_functions = num_hash_functions
         self.hash_cutoff = num_hash_functions / 2
         self.duplicate_cutoff = num_hash_functions * 3 / 4
@@ -40,8 +41,8 @@ class hash_family():
         for h, table in zip(self.hash_functions, self.hash_tables):
             yield h, table
 
-
     def add_to_hash_tables(self, to_hash, to_add):
+        ''' adds to_add in place that to_hash hashes, for all hash functions '''
         for h, table in zip(self.hash_functions, self.hash_tables):
             table[h[to_hash]] = list(set([val for val in table[h[to_hash]] if val not in self.marked]))
             if to_add not in table[h[to_hash]]:
@@ -53,7 +54,8 @@ class hash_family():
             self.max_bucket = max(self.max_bucket, len(table[h[to_hash]]))
 
 
-    def pretty_print(self):
+    def __repr__(self):
+        ''' prints all nonzero buckets, with elements, for each hash table '''
         for index, table in enumerate(self.hash_tables):
             print('Table', index)
             for bucket, elements in table.items():
@@ -62,94 +64,82 @@ class hash_family():
 
 
 class data():
-    def __init__(self, filename, num_phrases=5, ngrams = [2, 3]):
+    def __init__(self, filename, num_hash_functions=128, num_buckets=400, ngrams=[2, 3]):
+        self.filename_full = filename.split('.')[0]
         self.filename = os.path.basename(filename).split('.')[0]
         self.time_filename = self.filename + '_time.txt'
-        self.num_phrases = num_phrases
         self.ngrams = ngrams
         self.time = 0
-        self.description = 'u_Description'
-        self.ad_id = 'ad_id'
 
         self.data = pandas.read_csv(filename)
+
+        # automatically determine relevant header names
+        descriptions = {'u_Description', 'description', 'body'}
+        indices = {'ad_id', 'index'}
+        self.description = set(self.data.columns).intersection(descriptions).pop()
+        self.ad_id = set(self.data.columns).intersection(indices).pop()
+
         if 'PostingDate' in self.data.columns:
             self.data.sort_values(by=['PostingDate']) # since ads not in order as they should be
-        else:
-            self.description = 'body'
         self.num_ads = len(self.data.index)
         self.cluster_graph = nx.DiGraph()
-        self.hashes = hash_family()
+        self.hashes = hash_family(num_hash_functions, num_buckets)
+
+
+    def remove_html(self, text):
+        if type(text) is not str: # nan
+            return ''
+
+        replace = [(r'\d+', ''), (r'[^\x00-\x7F]+', ' '), (re.compile('<.*?>'), '')]
+        for source, target in replace:
+            text = re.sub(source, target, text)
+
+        return text
 
 
     def preprocess_ad(self, ad):
-        #return word_tokenize(ad)
-        try:
-            return word_tokenize(''.join([c.lower() for c in ad if c.isalnum() or c == ' ']))
-        except:
-            return word_tokenize('')
+        return word_tokenize(self.remove_html(ad))
 
-    def get_tokens(self, ad_text):
+
+    def get_tokens(self, ad_text, give_index=False):
         ad_text = self.preprocess_ad(ad_text)
         for ngram in self.ngrams:
-            for token in list(zip(*[ad_text[i:] for i in range(ngram)])):
-                yield token
-
-    def find_idf(self):
-        save_path = 'pkl_files/{}_idf.pkl'.format(self.filename)
-        if os.path.exists(save_path):
-            self.idf = pickle.load(open(save_path, 'rb'))
-            return
-
-        print('Finding idf...')
-        self.idf = Counter()
-        for _, row in self.data.iterrows():
-            for phrase in self.get_tokens(row[self.description]):
-                self.idf[phrase] += 1
-
-        for phrase, doc_freq in self.idf.items():
-            self.idf[phrase] = math.log10(self.num_ads/self.idf[phrase])
-
-        pickle.dump(self.idf, open(save_path, 'wb'))
+            for index, token in enumerate(list(zip(*[ad_text[i:] for i in range(ngram)]))):
+                if give_index:
+                    yield index, token
+                else:
+                    yield token
 
 
-    def tfidf(self, word, document):
-        tokens = list(zip(*[document[i:] for i in range(len(word))]))
-        return tokens.count(word) / len(tokens) * self.idf[word]
+    def find_tfidf(self):
+        ''' pre-calculate tfidf '''
+        print('Finding tfidf...')
+        stop_words = set(stopwords.words('english'))
+        vectorizer = TfidfVectorizer(lowercase=False, ngram_range=self.ngrams, norm='l2',
+                smooth_idf=True, stop_words=stop_words, min_df=2, max_df=0.8)
+        data = self.data[self.description].apply(self.remove_html)
+        self.tfidf = vectorizer.fit_transform(data)
+        self.tfidf_indices = vectorizer.get_feature_names()
+        self.tokenizer = vectorizer.build_tokenizer()
 
 
-    def calc_tfidf(self, ad_text, return_all=False):
-        scores = []
-        for ngram in self.ngrams:
-            tokens = list(zip(*[ad_text[i:] for i in range(ngram)]))
-            for index, phrase in enumerate(tokens):
-                score = self.tfidf(tuple(phrase), ad_text)
-                scores.append((score, index, phrase))
+    def calc_tfidf(self, index, return_all=False):
+        ''' return the top phrases with highest tfidf score '''
+        ad_tokens = self.preprocess_ad(self.data.loc[index][self.description])
 
-        scores.sort()
-        if return_all:
-            return scores
+        doc_tfidf = self.tfidf[index]
+        _, indices = doc_tfidf.nonzero()
 
-        # filter scores so that they take non-overlapping ngrams
-        filtered_scores = []
-        used_indices = set()
-        deleted_phrases = []
-        while len(filtered_scores) < self.num_phrases:
-            if not len(scores) and not len(deleted_phrases):
-                break
+        tfidf_sorted = sorted([(self.tfidf[index, i], self.tfidf_indices[i]) for i in indices], reverse=True)
+        total_len = 0
+        num_phrases = 1
 
-            if not len(scores):
-                filtered_scores.append(deleted_phrases.pop(0))
-                continue
+        # determine number of phrases to return: want 50% of ad length
+        while total_len < math.ceil(len(ad_tokens)) and num_phrases < len(tfidf_sorted):
+            total_len += len(tfidf_sorted[num_phrases][1])
+            num_phrases += 1
 
-            score, index, phrase = scores.pop()
-            if any([i in used_indices for i in range(index, index+len(phrase))]):
-                deleted_phrases.append((score, index, phrase))
-                continue
-
-            filtered_scores.append((score, index, phrase))
-            used_indices.update(range(index, index + len(phrase)))
-
-        return filtered_scores
+        return tfidf_sorted[:num_phrases]
 
 
     def find_related_clusters(self, phrases, ad_id):
@@ -178,15 +168,18 @@ class data():
 
 
     def add_new_cluster(self, related_clusters, cluster_id, ad_id):
+        ''' add cluster to cluster_graph, with related edges '''
         self.cluster_graph.add_node(cluster_id, contains=list([ad_id]))
         self.cluster_graph.add_edges_from([(rel, cluster_id) for rel in related_clusters])
 
 
-    def process_ad(self, row):
+    def process_ad(self, index, row):
+        ''' find top phrases, use them to find related clusters, and add the ad to the cluster
+        graph '''
         ad_text = self.preprocess_ad(row[self.description])
         ad_id = row[self.ad_id]
 
-        top_tfidf = [phrase for _, _, phrase in self.calc_tfidf(ad_text)]
+        top_tfidf = [phrase for _, phrase in self.calc_tfidf(index)]
         related_clusters, cluster_type, cluster_id = self.find_related_clusters(top_tfidf, ad_id)
 
         if cluster_type == 'duplicate':
@@ -200,14 +193,26 @@ class data():
 
 
     def write_cluster_graph(self):
+        ''' write cluster graph as pkl file '''
         with open('pkl_files/{}_ad_graph.pkl'.format(self.filename), 'wb') as f:
             pickle.dump(self.cluster_graph, f)
 
 
+    def write_csv_labels(self):
+        ''' write new csv, with LSH labels '''
+        my_labels = [0]*len(self.data.index)
+        for i, cluster in enumerate(nx.weakly_connected_components(self.cluster_graph)):
+            for ad in self.get_ads(cluster):
+                my_labels[ad] = i
+        self.data['LSH label'] = my_labels
+        self.data.to_csv(self.filename_full + '_LSH_labels.csv')
+
+
     def clustering(self):
+        ''' process each ad individually and incrementally save cluster graph. '''
         t = time.time()
-        self.find_idf()
-        print('Finished idf in time:', time.time() - t)
+        self.find_tfidf()
+        print('Finished tfidf in time:', time.time() - t)
         print('Starting clustering...')
 
         # assume in order of timestamp (streaming case)
@@ -221,14 +226,16 @@ class data():
                 with open(self.time_filename, 'a') as f:
                     f.write('{} {}\n'.format(index, time_elapsed))
 
-            self.process_ad(row)
+            self.process_ad(index, row)
 
         self.write_cluster_graph()
+        self.write_csv_labels()
+        self.total_time = time.time() - t
         print('Finished clustering!', time.time() - t)
-        #self.visualize_buckets()
 
 
     def visualize_buckets(self):
+        ''' creates visual representation of how full the buckets are for a hash table '''
         print('Plotting hash tables...')
         save_path = './plots/streaming_alg/' + self.filename
         print(save_path)
@@ -251,6 +258,51 @@ class data():
             plt.clf()
 
 
+    def get_clusters(self):
+        ''' given cluster graph, return the relevant connected components '''
+        criteria = lambda x: len(x) > 1 or len(self.get_ads(x)) > 1
+        return [c for c in nx.weakly_connected_components(self.cluster_graph) if criteria(c)]
+
+
+    def get_ads(self, cluster_nodes):
+        ''' given a set of cluster nodes, return the ads they represent '''
+        return [ad for node in cluster_nodes for ad in self.cluster_graph.nodes[node]['contains']]
+
+
+    def print_clusters(self):
+        print('number of clusters', len(self.get_clusters()))
+        for i, cluster in enumerate(self.get_clusters()):
+            print('cluster', i)
+            print('contains cluster_ids', cluster)
+            print(list(self.get_ads(cluster)))
+            for ad_id in self.get_ads(cluster):
+                try:
+                    description = self.remove_html(self.data.loc[ad_id][self.description])
+                except:
+                    print('issue with ad_id', ad_id, 'and desc', self.description)
+                true_label = self.data.loc[ad_id]['label']
+                print(true_label, description)
+                print()
+            print('\n\n')
+
+
+    def compare_true_synthetic(self):
+        true_labels = self.data['label'].values
+
+        my_labels = [0]*len(self.data.index)
+        for i, cluster in enumerate(nx.weakly_connected_components(self.cluster_graph)):
+            for ad in self.get_ads(cluster):
+                my_labels[ad] = i
+
+        nmi = normalized_mutual_info_score(true_labels, my_labels)
+        hom = homogeneity_score(true_labels, my_labels)
+        ari = adjusted_rand_score(true_labels, my_labels)
+        print('NMI        ', nmi)
+        print('HOMOGENEITY', hom)
+        print('RAND INDEX ', ari)
+        return nmi, hom, ari
+
+
 def usage(exit_code):
     print('Usage: _ [filename]')
     exit(exit_code)
@@ -260,6 +312,21 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         usage(1)
 
+    #filename = sys.argv[1]
+    #canadian_data = data(filename)
+    #name = 'pkl_files/{}_ad_graph.pkl'.format(canadian_data.filename)
+    #run_parameter_experiment()
     filename = sys.argv[1]
-    canadian_data = data(filename)
-    canadian_data.clustering()
+    hf, nb, n1, n2 = map(int, sys.argv[2:])
+    d = data(filename, num_hash_functions=hf, num_buckets=nb, ngrams=(n1, n2))
+    d.clustering()
+    score = d.compare_true_synthetic()
+    #d.print_clusters()
+    experiment_filename = os.path.basename(filename).split('.')[0] + '_paramTFIDF.txt'
+    with open(experiment_filename, 'a') as f:
+        f.write('{} {} {} {} {} {} {} {}\n'.format(hf, nb, n1, n2, d.total_time, *score))
+
+
+    #canadian_data.cluster_graph = pickle.load(open(name, 'rb'))
+    #canadian_data.clustering()
+    #canadian_data.compare_true_synthetic()
